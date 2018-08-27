f1921ab19c15b55716d5f262500398cc7d1c07fb
Steve Simon <mail@pmean.com>
2018-08-27 14:22:12 -0500

    Updated move-output-to-results to list selections.

--- src/move-output-to-results-folder.R
+++ src/move-output-to-results-folder.R
@@ -1,7 +1,9 @@
 project_name <- "survival-models"
 src <- paste("~", project_name, "src", sep="/")
 results <- paste("~", project_name, "results", sep="/")
-for (i in list.files(src, pattern="*.html")) {
-  file.copy(paste(src, i, sep="/"), results)
+html_list <- list.files(src, pattern="*html")
+for (i in html_list) {
+  file.copy(paste(src, i, sep="/"), results, overwrite=TRUE)
   file.remove(paste(src, i, sep="/"))
 }
+cat("\n\n", length(html_list), " file(s) moved: ", paste(html_list, collapse=", "), sep="")
