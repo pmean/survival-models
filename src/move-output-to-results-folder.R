11c6d433bae67100713b62e13ab97b2ad2f7bbde
Steve Simon <mail@pmean.com>
2020-11-01 10:28:52 -0600

    Major reorganization

--- src/move-output-to-results-folder.R
+++
@@ -1,9 +0,0 @@
-project_name <- "survival-models"
-src <- paste("~", project_name, "src", sep="/")
-results <- paste("~", project_name, "results", sep="/")
-html_list <- list.files(src, pattern="*html")
-for (i in html_list) {
-  file.copy(paste(src, i, sep="/"), results, overwrite=TRUE)
-  file.remove(paste(src, i, sep="/"))
-}
-cat("\n\n", length(html_list), " file(s) moved: ", paste(html_list, collapse=", "), sep="")
